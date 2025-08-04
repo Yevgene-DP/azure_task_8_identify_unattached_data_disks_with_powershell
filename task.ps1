3c4ca78c42549552f6bef20c38143e57473d78a0
Yevgene-DP <ebalalay@gmail.com>
2025-08-04 21:37:53 +0300

    Identify unattached disks and export to JSON

--- task.ps1
+++ task.ps1
@@ -1,1 +1,14 @@
+
+$resourceGroup = "mate-azure-task-5"
+$outputPath = "result.json"
+
+$disks = Get-AzDisk -ResourceGroupName $resourceGroup
+
+$unattachedDisks = $disks | Where-Object { !$_.ManagedBy }
+
+$diskNames = $unattachedDisks | Select-Object -ExpandProperty Name
+
+$diskNames | ConvertTo-Json -Depth 1 | Out-File -Encoding UTF8 $outputPath
+
+Write-Host "✅ Unattached disks saved to $outputPath"
 # Write your code here
